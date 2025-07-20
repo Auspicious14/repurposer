0f94b21b6307349958241d8923e16fd2edd92b92
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-20 06:43:59 +0100

    Update page.tsx

--- src/modules/auth/signup/page.tsx
+++ src/modules/auth/signup/page.tsx
@@ -17,6 +17,10 @@ const FormSchema = Yup.object().shape({
     .min(8, "Password must be at least 8 characters")
     .max(20, "Password must be at most 20 characters")
     .required("Password is required"),
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"
+    ),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords must match")
     .required("Confirm password is required"),
