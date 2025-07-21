b18456b6940be2b8e993035ca4a5a72c4735f56a
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-21 14:11:57 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
-import "../styles/globals.css";
-import { Toaster } from "sonner";
 import { AppContextProvider } from "@/context";
+import RootLayout from "@/modules/layout"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContextProvider>
-      <ThemeProvider attribute="class">
+      <RootLayout
+        <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
+      </RootLayout>
     </AppContextProvider>
   );
 }
