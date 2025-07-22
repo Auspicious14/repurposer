ea8ffdf82542711fc6362bbee8018d8791ca3648
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-22 15:57:54 +0100

    Update index.tsx

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,23 +1,9 @@
 import { GetServerSideProps } from "next";
 import { DashboardPage } from "@/modules/dashboard/page";
+import { withProtectedAuth } from "@/utils/auth"
 
 export default function Dashboard() {
   return <DashboardPage />;
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { req } = context;
-  const token = req.cookies?.token;
-
-  // if (!token) {
-  //   return {
-  //     redirect: {
-  //       destination: "/login",
-  //       permanent: false,
-  //     },
-  //   };
-  // }
-  return {
-    props: {},
-  };
-};
+export const getServerSideProps = withProtectedAuth();
