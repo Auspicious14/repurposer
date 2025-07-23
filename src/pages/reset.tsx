be3772127d6f37451d38b824a2e0eda207af166c
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-23 14:08:26 +0100

    Update reset.tsx

--- src/pages/reset.tsx
+++ src/pages/reset.tsx
@@ -1,4 +1,4 @@
-import { ResetPasswordPage } from "@/modules/auth/resetPassword"
+import { ResetPasswordPage } from "@/modules/auth/resetPassword/page"
 
 export default function ResetPassword() {
 return (
