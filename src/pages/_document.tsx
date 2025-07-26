2f369b31b040f41a32333e25869a09f709b8004a
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-07-26 15:23:02 +0100

    docs(_document): update meta description and keywords content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,23 +6,13 @@ export default function Document() {
       <Head>
         <meta
           name="description"
-          content="Forma: Transform audio transcripts into engaging content for various platforms."
+          content="Forma: Transform text, audio and video transcripts into engaging content for various platforms."
         />
         <meta
           name="keywords"
-          content="forma, content generation, AI, audio, video or text transcript, social media, blog, youtube, linkedin, twitter"
+          content="forma, content generation, AI, text, audio, or video transcript, social media, blog, youtube, linkedin, twitter, x, whatsapp"
         />
         <meta name="author" content="Abdulganiyu Uthman (Auspicious)" />
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=OpenSans:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        /> */}
       </Head>
       <body className="antialiased font-sans">
         <Main />
